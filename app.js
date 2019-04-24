61f930314757593d8badcc047e7a821a35287e60
hanshinkoko <hanna492019@gmail.com>
2019-04-24 17:14:09 +0630

    Project init

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mongoose = require('mongoose');
+var session = require('express-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -13,13 +14,35 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+mongoose.connect('mongodb+srv://myatthu:Myat001@nodetest-03isr.mongodb.net/test?retryWrites=true');
+var db = mongoose.connection;
+db.on('error',console.error.bind(console,'MongoDB connection error:'));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: '3f3ofdjfk!hi2#*&@Y*&GU',
+  resave: false,
+  saveUnintialized: true
+}));
+
+app.use(function (req,res,next) {
+  res.locals.use = req.session.use;
+  next();
+});
+
 app.use('/', indexRouter);
+app.use(function (req,res,next) {
+    if(req.session.user){
+      next();
+    }else{
+      res.redirect('/signin');
+    }
+});
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
---
+++ model/User.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var UserSchema = new Schema({
+  firstName:{ type:String, required:true},
+  lastName:{ type:String, required:true},
+  email:{ type:String, required:true},
+  city:{ type:String, required:true},
+  state:{ type:String, required:true},
+  zip:{ type:String, required:true}
+});
+module.exports = mongoose.model('Users',UserSchema);
